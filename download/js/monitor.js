9b19167e58f54beb6275ecd1ed5008fe66095aae
wangjiji-max <51913135+wangjiji-max@users.noreply.github.com>
2019-06-29 20:16:49 +0800

    Add files via upload

---
+++ download/js/monitor.js
@@ -0,0 +1,84 @@
+﻿var monitor = {
+    _domain: "monitor.cheyipai.com",
+    _sessionName: "cypweblog",
+    _pageId: "",
+    _monitorModel: "",
+    _http: "http://",
+    Init: function () {
+        $('.track').click(function () { monitor.TrackClick(this); });
+    },
+    TrackClick: function (obj) {
+        var name = $(obj).attr("trackname");
+        var sessionId = monitor.GetCookie();
+        var url = window.location.href;
+        var referrer = document.referrer;
+        var postUrl = monitor._http + monitor._domain + "/click";
+        $.ajax({
+            url: postUrl,
+            dataType: "jsonp",
+            data: { url: url, referrer: referrer,name: name, sessionId: sessionId, monitorModel: monitor._monitorModel },
+            jsonp: "jsonpcallback"
+        });
+    },
+    LoadPage: function (monitorModel,ishttps) {
+        monitor._pageId = monitor.GetGuid();
+        monitor.Init();
+        try {
+            $(window).unload(function () {
+                monitor.Leave();
+            });
+        } catch (e) {
+
+        }
+        if (ishttps == true) {
+            monitor._http = "https://";
+        }
+        var sessionId = monitor.GetCookie();
+        var screenHeight = window.screen.height;
+        var screenWidth = window.screen.width;
+        var url = window.location.href;
+        var referrer = document.referrer;
+        var postUrl = monitor._http + monitor._domain + "/loadpage";
+        monitor._monitorModel = monitorModel;
+        $.ajax({
+            url: postUrl,
+            dataType: "jsonp",
+            data: { url: url, referrer: referrer, sessionId: sessionId, pageId: monitor._pageId, monitorModel: monitorModel },
+            jsonp: "jsonpcallback"
+        });
+    },
+    Leave: function () {
+        var sessionId = monitor.GetCookie();
+        var screenHeight = window.screen.height;
+        var screenWidth = window.screen.width;
+        var url = window.location.href;
+        var referrer = document.referrer;
+        var postUrl = monitor._http + monitor._domain + "/leave";
+        $.ajax({
+            url: postUrl,
+            dataType: "jsonp",
+            data: { url: url, referrer: referrer, sessionId: sessionId, pageId: monitor._pageId, monitorModel: monitor._monitorModel },
+            jsonp: "jsonpcallback"
+        });
+    },
+    //写cookies
+    SetCookie: function () {
+        var guid = monitor.GetGuid();
+        document.cookie = monitor._sessionName + "=" + escape(guid);
+        return guid;
+    },
+    //读取cookies
+    GetCookie: function () {
+        var arr, reg = new RegExp("(^| )" + monitor._sessionName + "=([^;]*)(;|$)");
+        if (arr = document.cookie.match(reg)) return unescape(arr[2]);
+        else {
+            return monitor.SetCookie();
+        }
+    },
+    GetGuid: function () {
+        var S4 = function () {
+            return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+        };
+        return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+    }
+};
---
+++ download/js/pcdownload.js
@@ -0,0 +1,32 @@
+﻿var cypSwiper = new Swiper('#downCont', {
+    loop: true,
+    onSlideChangeEnd: function (cypSwiper) {
+        var index = cypSwiper.realIndex;
+        $(".down_nav li").removeClass("active").eq(index).addClass("active");
+    }
+});
+
+$(".down_nav li").click(function () {
+    var i = $(this).index() + 1;
+    cypSwiper.slideTo(i, 500, false);
+    $(this).addClass("active").siblings().removeClass("active");
+});
+
+
+$(function () {
+    $(".androidDown").each(function() {
+        $(this).click(function () {
+            var downUrl = $(this).attr("url");
+            $("#AndroidUrl").attr("href", downUrl);
+            $("#spAndroid").click();
+        });
+    });
+
+    $(".iosDown").each(function() {
+        $(this).click(function () {
+            var downUrl = $(this).attr("url");
+            $("#IosUrl").attr("href", downUrl);
+            $("#spIos").click();
+        });
+    });
+});
