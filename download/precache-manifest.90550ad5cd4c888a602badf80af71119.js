77f6827615ff7816da821f3dc8a4a3b3d7ebb100
wangjiji-max <51913135+wangjiji-max@users.noreply.github.com>
2019-07-08 01:34:28 +0800

    Add files via upload

---
+++ download/precache-manifest.90550ad5cd4c888a602badf80af71119.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa362319a374607e2762a904a4abd0a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7fbdf7f5ec87ee4e99de",
+    "url": "./static/css/2.6d26e6ff.chunk.css"
+  },
+  {
+    "revision": "c0114e8f61ad44813864",
+    "url": "./static/css/main.ae3ad04f.chunk.css"
+  },
+  {
+    "revision": "7fbdf7f5ec87ee4e99de",
+    "url": "./static/js/2.038d7343.chunk.js"
+  },
+  {
+    "revision": "c0114e8f61ad44813864",
+    "url": "./static/js/main.c10cde84.chunk.js"
+  },
+  {
+    "revision": "29f0742aa03144191e2b",
+    "url": "./static/js/runtime~main.290cf2e6.js"
+  },
+  {
+    "revision": "b474d3e4066a071bed281bcceab384f7",
+    "url": "./static/media/bg@2x.b474d3e4.png"
+  },
+  {
+    "revision": "d5058533a88cccd54576200ccdb23f7c",
+    "url": "./static/media/footer-bg.d5058533.png"
+  },
+  {
+    "revision": "3ae2a6a9978f2c1a5342189c6e113d0e",
+    "url": "./static/media/fot-bg.3ae2a6a9.png"
+  },
+  {
+    "revision": "dcf5a351f7f322c43735cf1efdd8e314",
+    "url": "./static/media/ft-bg.dcf5a351.png"
+  },
+  {
+    "revision": "2c0f8f1980507a70af0b257ed8788ee8",
+    "url": "./static/media/jsbg.2c0f8f19.png"
+  },
+  {
+    "revision": "df683a274924bb3fc4d70c403060931d",
+    "url": "./static/media/p-04.df683a27.png"
+  },
+  {
+    "revision": "dead857aaadefd3e1805148764016848",
+    "url": "./static/media/v2@x.dead857a.png"
+  }
+]);
